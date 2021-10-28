660aebf0226b3adb5dc37dc38252b68f73382699
Izaakwltn <75645156+Izaakwltn@users.noreply.github.com>
2021-10-28 12:07:36 -0600

    Update index.lisp

--- www/index.lisp
+++ www/index.lisp
@@ -1,17 +1,17 @@
 ;;;;index.lisp
 
-(in-package :scale-your-violin)
+;(in-package :scale-your-violin)
 
-(defun index-page ()
-  (with-page (:title "Scale Your Violin")
-    (:header
-     (:h1 "Scale Your Violin"))
-    (:section
-     (:p "Hello, welcome to Scale Your Violin, a virtual violin studio dedicated to helping students find their best sound, fastest progress, and most effective technique possible!
+;(defun index-page ()
+ ; (with-page (:title "Scale Your Violin")
+  ;  (:header
+   ;  (:h1 "Scale Your Violin"))
+    ;(:section
+    ; (:p "Hello, welcome to Scale Your Violin, a virtual violin studio dedicated to helping students find their best sound, fastest progress, and most effective technique possible!
 
-    Look around our site, or get in touch:
-    izaakviolin@gmail.com"))))
+;    Look around our site, or get in touch:
+ ;   izaakviolin@gmail.com"))))
 
-(push
- (hunchentoot:create-prefix-dispatcher "/index.html" #'index-page)
- hunchentoot:*dispatch-table*)
+;(push
+ ;(hunchentoot:create-prefix-dispatcher "/index.html" #'index-page)
+ ;hunchentoot:*dispatch-table*)
