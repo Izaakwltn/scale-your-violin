cdad7b3ec23f2bcaa3340635974ed5b76327ccf1
Izaakwltn <75645156+Izaakwltn@users.noreply.github.com>
2021-10-29 11:45:50 -0600

    Updated CSS and nav bar

--- scale-your-page.lisp
+++ scale-your-page.lisp
@@ -10,15 +10,14 @@
        (:head
 	(:meta :charset "utf-8")
 	(:meta :name "viewport" :content "width=device-width, initial-scale=1")
-	(:link :href "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-	       :rel "stylesheet"
-	       :integrity "sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-	       :crossorigin "anonymous")
-         (:title ,title)
+	(:link :rel "stylesheet"
+	       :href "https://cdn.simplecss.org/simple.min.css")
+        (:title ,title))
+       (:header
 	 (:nav
-	  (:button (:a :href "index.html" "Scale Your Violin"))
-	  (:button (:a :href "contact.html" "contact"))
-	  (:button (:a :href "teachers.html" "teachers"))
-	  (:button (:a :href "resonance.html" "Resonance Calculator"))))
+	  (:a :style "color:black;" :href "index.html" "Scale Your Violin")
+	  (:a :href "contact.html" "Contact")
+	  (:a :href "teachers.html" "Teachers")
+	  (:a :href "resonance.html" "Resonance Calculator")))
          (:body ,@body)
-	 (:footer "Scale Your Violin" (:a :href "https://www.github.com/scale-your-violin" "code at github.com")))))
+	 (:footer "Scale Your Violin" (:a :href "https://www.github.com/Izaakwltn/scale-your-violin" "code at github.com")))))
